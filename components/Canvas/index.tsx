d4257979154b55de8b1e6a90f013523795dcd101
ZHKO1 <zhko11993@gmail.com>
2025-01-21 07:14:07 +0000

    feat: 布局

---
+++ components/Canvas/index.tsx
@@ -0,0 +1,8 @@
+export default function Canvas() {
+    return (
+      <div className="w-full h-full flex items-center justify-center text-4xl font-bold text-white">
+        FUCKYOU
+      </div>
+    );
+  }
+  
---
+++ components/HeaderBar/index.tsx
@@ -0,0 +1,8 @@
+export default function HeaderBar() {
+    return (
+      <div className="w-full h-full flex items-center justify-center text-4xl font-bold text-black">
+        SHIT DAMN
+      </div>
+    );
+  }
+  
