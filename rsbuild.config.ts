f9bc1b08b4b9fcdecbc5681f6c938c315836b74b
ZHKO1 <zhko11993@gmail.com>
2025-03-18 11:18:34 +0000

    feat: title and favicon

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  html: {
+    title: '週刊少年ジャンプ',
+    favicon: './public/icon.jpg',
+  },
   resolve: {
     alias: {
       '@': '.',
