67c32033d0c0227171b4a5f7613aa00f83125a8b
ZHKO1 <zhko11993@gmail.com>
2025-03-19 12:02:42 +0000

    chore: add @antfu/eslint-config simple-git-hooks lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import antfu from '@antfu/eslint-config'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default antfu(
   {
-    files: ["**/*.ts", "**/*.tsx"],
+    react: true,
+    type: 'app',
+  },
+  {
+    ignores: ['components/ui', "*.config.ts", "*.config.mjs", "tsconfig.json", "components.json"],
+  },
+  {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+      "style/no-mixed-operators": "off"
     }
   }
-];
-
-export default eslintConfig;
+)
